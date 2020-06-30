e566798da6112c2b08c7265826db6834495850b0
Pauli <andrarias@gmail.com>
2020-06-30 14:55:11 -0300

    actualizndoAngular

--- AngularRecipes/src/app/shared/login/login.component.ts
+++ AngularRecipes/src/app/shared/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthGuard } from '../guards/auth.guard';
+/* Para autenticar */
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
 
 @Component({
   selector: 'app-login',
@@ -9,13 +13,29 @@ import { AuthGuard } from '../guards/auth.guard';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router, private authServ: AuthGuard) { }
+  /* constructor(private router: Router,
+              private authServ: AuthGuard,
+              public afAuth: AngularFireAuth) { } */
+
+  constructor(private router: Router,
+              private authServ: AuthGuard
+             ) { }
 
   ngOnInit() {
   }
 
+  onLogingGoogle() {
+   
+    /* this.afAuth.signInWithPopup(new auth.GoogleAuthProvider ());
+    this.router.navigate(['/test']); */
+  }
+
   onLogin() {
     this.authServ.logIn();
+  }
+
+  onLogoutGoogle() {
+    // this.afAuth;
   }
   
   onLogout() {
